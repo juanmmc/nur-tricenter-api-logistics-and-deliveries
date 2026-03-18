14b2fb5f9983508f4a34e27d736d8858ac862353
Juan Miguel Muriel Céspedes <juanmuriel@uagrm.edu.bo>
2026-03-17 23:45:31 -0400

    test: add hook commit-msg

---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'chore',
+                'docs',
+                'refactor',
+                'test',
+                'ci',
+                'perf',
+                'build'
+            ]
+        ],
+        'header-max-length': [2, 'always', 100]
+    },
+};
